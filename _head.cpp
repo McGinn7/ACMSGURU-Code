2dcaa68863deebd78b1e970fd2071186113c6e32
mcginn7 <958032738@qq.com>
2019-12-02 11:59:31 +0800

    add _head.cpp

---
+++ _head.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef double db;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) begin(x),end(x)
+#define rep(i,l,r) for(int i=(l);i<(r);++i)
+#define per(i,l,r) for(int i=(r)-1;i>=(l);--i)
+#define dd(x) cout << #x << "=" << x << ", "
+#define de(x) cout << #x << "=" << x << endl
+//-------
